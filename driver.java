2417f2407fd6e0aafb8897f71a5a13b1a17208fe
rustumsk <rustumarellano@gmail.com>
2023-11-25 20:56:23 +0800

    added the needed directories

---
+++ driver.java
@@ -0,0 +1,7 @@
+package OOP_Project;
+import OOP_Project.model.*;
+public class driver{
+    public static void main(String[] args){
+        tri.hello();
+    }
+}
---
+++ model/tri.java
@@ -0,0 +1,7 @@
+package OOP_Project.model;
+
+public class tri {
+    public static void hello(){
+        System.out.println("Hello World");
+    }
+}
